0ad6710553c85601e6987f23134c59c9ad5dd981
elv1234vain <elvainndizeye@gmail.com>
2025-12-23 12:45:53 +0200

    first commit

---
+++ nested.js
@@ -0,0 +1,17 @@
+
+// for (let i = 1; i <= 5; i++) {
+//     let star = ""
+//     for (let j = 1; j <= i; j++) {
+//         star += "*"
+//     }
+//     console.log(star)
+// }
+
+let row = 5;
+for(let num = 1; num <= row; num++){
+    let star1 ='';
+    for (let nume = 1; nume <= num; nume++){
+        star1 += "*"
+    }
+console.log(star1)
+}
